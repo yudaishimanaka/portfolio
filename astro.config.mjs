34dc98bd8c5ba92c2b50f6eded930ee15da76dff
yudaishimanaka <gurutaminn.h8@gmail.com>
2025-11-13 10:40:27 +0900

    ドメイン定義を修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yudai.dev',
+  site: 'https://www.yudai.dev',
   integrations: [mdx(), tailwind(), sitemap()]
 });
