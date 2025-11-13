5781926928fe059da9aff3ff8d243eec5af9ac4b
yudaishimanaka <gurutaminn.h8@gmail.com>
2025-11-13 10:32:26 +0900

    robots.txtとサイトマップの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  site: 'https://yudai.dev',
+  integrations: [mdx(), tailwind(), sitemap()]
 });
