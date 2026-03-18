16a50230df00426b78f04b23b9c5d7da31402c0b
yudaishimanaka <gurutaminn.h8@gmail.com>
2026-03-18 23:14:57 +0900

    astro6への移行のためastrojs/mdxを削除しtailwindcssをvite公式プラグインからの利用に変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import fontOptimizer from './src/integrations/font-optimizer.ts';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.yudai.dev',
-  integrations: [mdx(), tailwind(), sitemap(), fontOptimizer()]
+  integrations: [sitemap(), fontOptimizer()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
